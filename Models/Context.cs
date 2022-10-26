3f6660eb0828e2681fb0abd7241dec6249f04f96
Caio Ferrari <83523721+Caioferrari04@users.noreply.github.com>
2022-10-26 11:29:17 -0300

    arquivos certos

---
+++ Models/Context.cs
@@ -0,0 +1,16 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace MvcLabManager.Models;
+
+public class Context : DbContext 
+{
+    public Context(DbContextOptions<Context> options) : base(options)
+    { }
+
+    // protected override void OnModelCreating(ModelBuilder modelBuilder) 
+    // {
+    //     modelBuilder.Entity<Computer>().Property(c => c.Id).ValueGeneratedOnAdd();
+    // }
+
+    public DbSet<Computer> Computers { get; set; }
+}
